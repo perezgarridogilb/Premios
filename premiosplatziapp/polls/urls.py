cfbda6a230deebd10e848720997b6ff1db598c49
Gilberto <Betho6990@gmail.com>
2022-01-27 22:32:08 -0600

    Forms: The basic

--- premiosplatziapp/polls/urls.py
+++ premiosplatziapp/polls/urls.py
@@ -2,13 +2,15 @@ from django.urls import path
 # Desde este paquete vamos a importar el archivo views
 from . import views
 
+# url + app_name + name
+app_name = "polls"
 urlpatterns = [
     # ex: /polls/
     path("", views.index, name="index"),
     # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="index"),
+    path("<int:question_id>/detail/estaeslamejorpagina", views.detail, name="detail"),
     # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="Results"),
+    path("<int:question_id>/results/", views.results, name="results"),
     # ex: /polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="Vote"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
