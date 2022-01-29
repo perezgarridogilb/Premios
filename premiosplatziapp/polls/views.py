86c042e74462de8b0d553e30768cf772bc841bcf
Gilberto <Betho6990@gmail.com>
2022-01-28 20:13:49 -0600

    Creating the results view

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+import re
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Function Based Views.
 
@@ -19,7 +21,26 @@ def detail(request, question_id):
     })
 
 def results(request, question_id):
-    return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {
+        "question": question
+    })
 
 def vote(request, question_id):
-    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    # Intentamos acceder a una llave que no existe    
+    except (KeyError, Choice.DoesNotExist):
+       return render(request, "polls/detail.html", {
+           "question": question,
+           "error_message": "No elegiste una respuesta"
+       })
+    else:
+        # Ya era un atributo que nosotros definimos en el modelo de choices
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Redirect: se asegura que el usuario no envíe la información dos veces en el formulario
+        # args: Argumentos en la URL
+        # return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
+        return redirect("polls:results", question.id)
